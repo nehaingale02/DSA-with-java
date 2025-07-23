e3e437dcbc10be92c2791a1f4aea870659c0b5d4
nehaingale02 <nehaingale02@gmail.com>
2025-07-23 20:21:12 +0530

    Multiply two Floating-Point Numbers

---
+++ src/javaBasicMathQs.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class javaBasicMathQs {
+    public static void main(String[] args) {
+      //  Java Program to Multiply two Floating-Point Numbers
+        float n1 = 3.4f;
+        float n2 = 5.8f;
+        float result = n1 * n2;// sc object take the input (from keyboard) of int type and holds
+        System.out.println("Multiplication of two numbers is: " + result); // prints the output on console screen
+    }
+}
